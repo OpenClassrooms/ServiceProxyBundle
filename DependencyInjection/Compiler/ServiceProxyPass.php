e1dec87eedfab96c78dd9c7daf92f6152c7354d1
Kévin Letord <kevin.letord@openclassrooms.com>
2017-05-10 14:23:07 +0200

    Add logging to ServiceProxyPass

--- DependencyInjection/Compiler/ServiceProxyPass.php
+++ DependencyInjection/Compiler/ServiceProxyPass.php
@@ -2,7 +2,9 @@
 
 namespace OpenClassrooms\Bundle\ServiceProxyBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Compiler\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\LoggingFormatter;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
@@ -17,9 +19,22 @@ class ServiceProxyPass implements CompilerPassInterface
      */
     private $container;
 
+    /**
+     * @var Compiler
+     */
+    private $compiler;
+
+    /**
+     * @var LoggingFormatter
+     */
+    private $formatter;
+
     public function process(ContainerBuilder $container)
     {
         $this->container = $container;
+        $this->compiler = $container->getCompiler();
+        $this->formatter = $this->compiler->getLoggingFormatter();
+
         $this->buildServiceProxies();
     }
 
@@ -31,6 +46,7 @@ class ServiceProxyPass implements CompilerPassInterface
         foreach ($taggedServices as $taggedServiceName => $tagParameters) {
             $this->buildServiceProxyFactoryDefinition($taggedServiceName, $tagParameters);
             $serviceProxyIds[] = $taggedServiceName;
+            $this->compiler->addLogMessage($this->formatter->format($this, 'Add proxy for ' . $taggedServiceName . ' service.'));
         }
         $this->container->setParameter('openclassrooms.service_proxy.service_proxy_ids', $serviceProxyIds);
     }
@@ -38,6 +54,7 @@ class ServiceProxyPass implements CompilerPassInterface
     private function buildServiceProxyFactoryDefinition($taggedServiceName, $tagParameters)
     {
         $definition = $this->container->findDefinition($taggedServiceName);
+
         $factoryDefinition = new Definition();
         $factoryDefinition->setFactory([new Reference('openclassrooms.service_proxy.service_proxy_factory'), 'create']);
         $factoryDefinition->setArguments([$definition, $tagParameters[0]]);
